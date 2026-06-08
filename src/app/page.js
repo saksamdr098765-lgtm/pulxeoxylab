285ece1f7c48e6424eeb59692e16e4f503ef9cb9
rohit <rohitdr098@gmail.com>
2026-06-08 11:23:13 +0530

    rediredted to my portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import Hero from "./components/home/hero";
 
@@ -7,9 +8,15 @@ import Statatics from "./components/home/Statatics";
 import CTA from "./components/home/CTA";
 import Footer from "./components/home/Footer";
 import DiagnosticPackages from "./components/home/Packages";
+import { useEffect } from "react";
+
 
 
 export default function Home() {
+  
+  useEffect(()=>{
+window.location.href = "https://www.rohitdevstack.online/";
+  },[])
   return (
  <>
  <Hero></Hero>
